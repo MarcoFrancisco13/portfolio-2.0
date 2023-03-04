f3b4c6dc21e47ab7651ff2a3a4119afa669a2b9b
MarcoFrancisco13 <franciscomarco2921@gmail.com>
2023-03-04 14:50:23 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portfolio 2.0`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "9OGmaY7CkeEUVV9O_ZTW2nRbyhQUXlcQ5I2EGTzEwZQ",
+      "spaceId": "byvz0lerhhcx"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+};
